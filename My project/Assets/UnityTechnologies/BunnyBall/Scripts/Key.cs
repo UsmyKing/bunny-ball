cb191a0ce14ca54e2bc733868f4b8a00d69bb4cb
UsmyKing <usmyg42@gmail.com>
2025-10-21 09:28:46 -0400

    Change speed

--- My project/Assets/UnityTechnologies/BunnyBall/Scripts/Key.cs
+++ My project/Assets/UnityTechnologies/BunnyBall/Scripts/Key.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    // This is a variable for the shackwave Prefab
     [SerializeField] GameObject shockwavePrefab;
+    // This is a variable for the game manager
     [SerializeField] GameManager gameManager;
 
+    // This function runs once at the beginning of the game
     private void Start()
     {
+        // Finds the gameManager
         gameManager = FindObjectOfType<GameManager>();
     }
+
+    // This function runs when we collide with the trigger
     private void OnTriggerEnter(Collider other)
     {
+        // checks if we collide with the player 
         if (other.CompareTag("Player")) {
+            // sets game over to true
             gameManager.gameOver = true;
+            //creates a shockwave 
             Instantiate(shockwavePrefab, transform.position, Quaternion.identity);
+            // destroys key 
             Destroy(gameObject, 0.1f);
         }
     }
 }
+ 
