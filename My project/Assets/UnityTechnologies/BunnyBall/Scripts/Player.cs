0b17137dc42923190bd1e711965960d1fecf8ff6
UsmyKing <usmyg42@gmail.com>
2025-10-24 09:23:06 -0400

    Jumpforce

--- My project/Assets/UnityTechnologies/BunnyBall/Scripts/Player.cs
+++ My project/Assets/UnityTechnologies/BunnyBall/Scripts/Player.cs
@@ -8,12 +8,13 @@ public class Player : MonoBehaviour
     public Transform cameraTransform;
     public GameManager gameManager;
     public  float speed = 10f;
+    public int jumpforce = 100;
     private int x = 0;
 
     void Update()
     {
         x = x + 1;
-        Debug.Log(message:"Hello" + x);
+        // Debug.Log(message:"Hello" + x);
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
@@ -25,6 +26,13 @@ public class Player : MonoBehaviour
         right.Normalize();
         Vector3 direction = forward * moveVertical + right * moveHorizontal;
         rb.AddForce(direction * speed);
+        
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space was pressed");
+            rb.AddForce(Vector3.up * jumpforce);
+        }
     }
 
 }
